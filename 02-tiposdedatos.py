f956589d367cc187dc9dcc2b16f4bb6fe94a721d
Víctor Saldivia Millalonco <saldiviavictor1925@gmail.com>
2026-05-12 15:52:08 -0400

    Subiendo actualización de variable de 02-tiposdedatos

--- 02-tiposdedatos.py
+++ 02-tiposdedatos.py
@@ -5,5 +5,48 @@
 # Números enteros 
 edad = 18
 
+# Números Flotantes (Reales)
+estatura = 1.75 # el deciaml se utiliza punto y no coma
+
+# Números complejos 
+num_complejo = 4 + 2j           # primera forma de crear un número complejo
+otro-complejo = complex (4,2)   # segunda forma de crear un número complejo
+
+
+print (num_complejo)
+print (otro_complejo)
+
+# Operación  Básica (Área de un Triángulo)
+base = 8
+altura = 12.5
+
+   
+área = (base * altura) / 2
+
+print = área 
+print (f"El área el triángulo es de {área} cm")
+
+
+
+# Formatos de Salida de Números
+print(f"El número PI tiene un valor de {PI: .4f}")
+
+
+# El método de Redondeo
+print (round(área))
+print (f"El área del triángulo es de {round(área)} cm")
+
+# Transformaciones de Números
+print (float(edad))
+
+
+# Cadenas de Texto
+carrera = "Ingeniería Civil en Informática"
+institución = "Universidad de los Lagos"
+
+print (carrera[0])
+
+
+
 
 
